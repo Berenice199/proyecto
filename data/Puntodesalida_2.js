c1d95f2e9675c7425923adb9b86947c4f93d9177
Berenice199 <86080368+Berenice199@users.noreply.github.com>
2021-06-25 13:59:50 -0500

    Initial commit

---
+++ data/Puntodesalida_2.js
@@ -0,0 +1,1 @@
+var json_Puntodesalida_2 = {"type":"FeatureCollection","name":"Puntodesalida_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Id":0.0},"geometry":{"type":"Point","coordinates":[-100.898475555253313,19.487262728595343]}}]}
